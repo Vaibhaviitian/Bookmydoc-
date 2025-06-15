d65b98c574a2f81c086ac51163319796718bc52f
Vaibhaviitian <aryanvaibhav74@gmail.com>
2025-06-16 00:43:56 +0530

    adad

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -52,7 +52,7 @@ const Login = () => {
 
   return (
     <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
-      <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg'>
+      <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-black text-sm shadow-lg'>
         <p className='text-2xl font-semibold'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</p>
         <p>Please {state === 'Sign Up' ? 'sign up' : 'log in'} to book appointment</p>
         {state === 'Sign Up'
@@ -72,8 +72,8 @@ const Login = () => {
         </div>
         <button className='bg-primary text-white w-full py-2 my-2 rounded-md text-base'>{state === 'Sign Up' ? 'Create account' : 'Login'}</button>
         {state === 'Sign Up'
-          ? <p>Already have an account? <span onClick={() => setState('Login')} className='text-primary underline cursor-pointer'>Login here</span></p>
-          : <p>Create an new account? <span onClick={() => setState('Sign Up')} className='text-primary underline cursor-pointer'>Click here</span></p>
+          ? <p>Already have an account? <span onClick={() => setState('Login')} className='text-blue-100 underline cursor-pointer'>Login here</span></p>
+          : <p>Create an new account? <span onClick={() => setState('Sign Up')} className='text-blue-100 underline cursor-pointer'>Click here</span></p>
         }
       </div>
     </form>
